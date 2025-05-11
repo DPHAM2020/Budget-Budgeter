9814d45c40fee3bdd3e04125d5890097c1b1599c
DPHAM2020 <dpham2020@berkeley.edu>
2025-05-11 13:50:22 -0700

    quick fix: User -> UserSerializer in views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,5 +7,5 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()      # List of objects we are going to look at when creating a new user (prevents duplicate users)
-    serializer_class = User            # Tells the view what kind of data we need to accept to make a new user
+    serializer_class = UserSerializer            # Tells the view what kind of data we need to accept to make a new user
     permission_classes = [AllowAny]    # Allows anyone (even unauthenticated users) to create new accounts

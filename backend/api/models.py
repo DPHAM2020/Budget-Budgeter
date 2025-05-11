a4792c3eb229558075ed02c390e34f24a2da9008
DPHAM2020 <dpham2020@berkeley.edu>
2025-05-10 20:53:47 -0700

    restructured files

---
+++ backend/api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Undecided on whether or not deleting a category would delete the transaction
+    date = models.DateField()
+    note = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.transaction_type}: ${self.amount} - {self.category.name}"
+    

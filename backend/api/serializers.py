3cbb88ccd416f95264431766058d888d373d717f
DPHAM2020 <dpham2020@berkeley.edu>
2025-05-11 20:52:48 -0700

    small changes, no real functionality added

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
+        fields = ["id", "username", "password", "first_name", "last_name"]
         extra_kwargs = {"password": {"write_only": True}}  # Optional; used to further customize a declared field
 
     def create(self, validated_data):
